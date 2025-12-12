80c0ec41918d4f9680a20b76bbde2548af356f6b
NipsuK07 <150115940+NipsuK07@users.noreply.github.com>
2025-12-12 08:57:50 +0200

    .json filu lisätty
    lisätty .json file että voi lisätä kuvat sivulle.

--- PokeAPi/index.js
+++ PokeAPi/index.js
@@ -1,7 +1,13 @@
 fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
     .then(responce => responce.json())
     .then(data => {
-        console.log(data);
+        console.log(data.name, " ID: ", data.id, " Korkeus: ", data.height, " Paino: ", data.weight );
+        document.getElementById("PokemonName").innerText = data.name;
+        document.getElementById("PokemonID").innerText = "ID: " + data.id;
+        document.getElementById("PokemonType").innerText = "Tyyppi: " + data.types.map(typeInfo => typeInfo.type.name).join(", ");
+        document.getElementById("PokemonAbilities").innerText = "Voimat: " + data.abilities.map(abilityInfo => abilityInfo.ability.name).join(", ");
+
+
     })
     .catch(error => {
         console.error("Virhe haettaessa:", error)
