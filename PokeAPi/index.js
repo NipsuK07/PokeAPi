5b2ed69e990c9e097df0d40e6be16ab268adb792
NipsuK07 <150115940+NipsuK07@users.noreply.github.com>
2025-12-15 14:28:42 +0200

    Styleä lisätty ja sain kuvat näkyviin

--- PokeAPi/index.js
+++ PokeAPi/index.js
@@ -1,13 +1,27 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+fetch("https://pokeapi.co/api/v2/pokemon/dragonite")
     .then(responce => responce.json())
     .then(data => {
         // console.log(data.name, " ID: ", data.id, " Korkeus: ", data.height, " Paino: ", data.weight);
         const img = document.getElementById("PokemonImage");
         img.src = data.sprites.other["official-artwork"].front_default;
-        document.getElementById("PokemonName").innerText = data.name;
+        document.getElementById("PokemonName").innerText = capitalizeFirstLetter(data.name);
+        const pokemonName = data.name
+        const displayName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+        console.log(data)
+        console.log(data.name);
+        
+        function capitalizeFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+        function spaceBetweenWords(string) {
+            return string.replace(/-/g, ' ');
+        }
+
         document.getElementById("PokemonID").innerText = "ID: " + data.id;
-        document.getElementById("PokemonType").innerText = "Tyyppi: " + data.types.map(typeInfo => typeInfo.type.name).join(", ");
-        document.getElementById("PokemonAbilities").innerText = "Voimat: " + data.abilities.map(abilityInfo => abilityInfo.ability.name).join(", ");
+
+        document.getElementById("PokemonType").innerText = "Type: " + data.types.map(typeInfo => typeInfo.type.name).join(", ");
+
+        document.getElementById("PokemonAbilities").innerText = spaceBetweenWords("Ability: " + data.abilities.map(abilityInfo => abilityInfo.ability.name).join(", "));
     })
     .catch(error => {
         console.error("Virhe haettaessa:", error)
